0caf4d583e660b56f31b0d1593a321058db69ee8
SREEYESWANTH-R <sreeyeswanthr@gmail.com>
2025-05-18 18:14:02 +0530

    User model creation

---
+++ app/models/userModel.tsx
@@ -0,0 +1,41 @@
+import mongoose, { models } from "mongoose";
+
+
+const userSchema = new mongoose.Schema({
+    name:{
+        type:String,
+        required:true
+    },
+    email:{
+        type:String,
+        required:true,
+        unique:true
+    },
+    mobile:{
+        type:Number,
+        required:true
+    },
+    address:{
+        type:String,
+        required:true
+    },
+    city:{
+        type:String,
+        required:true
+    },
+    state:{
+        type:String,
+        required:true
+    },
+    pincode:{
+        type:String,
+        required:true
+    },
+    password:{
+        type:String,
+        required:true
+    },
+})
+
+const User = models.users || mongoose.model('users',userSchema);
+export default User;
