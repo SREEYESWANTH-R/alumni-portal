85ea0c3ba9c5e5bd81424434a1239e3ab3b8da2a
SREEYESWANTH-R <sreeyeswanthr@gmail.com>
2025-05-18 18:19:48 +0530

    jwt token generate function

---
+++ app/lib/generateToken.tsx
@@ -0,0 +1,19 @@
+import jwt from "jsonwebtoken";
+
+const JWT_SECRET = process.env.JWT_SECRET;
+
+if (!JWT_SECRET) {
+  throw new Error("JWT_SECRET is not defined in environment variables.");
+}
+
+interface UserPayload {
+  _id: string;
+  email: string;
+}
+
+export const generateToken = (user: UserPayload): string => {
+  return jwt.sign(
+    { id: user._id, email: user.email, username: user.name },
+    JWT_SECRET
+  );
+};
