67d06c93eb4000de2cb80557166dc8b362f645c4
SREEYESWANTH-R <sreeyeswanthr@gmail.com>
2025-05-18 18:08:40 +0530

    Overall Layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,9 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Toaster } from "@/components/ui/sonner"
 import "./globals.css";
+import Header from "@/app/component/Header";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -24,10 +17,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className="px-5 py-1 md:px-10 md:py-2">
+        <Header/>
         {children}
+        <Toaster/>
       </body>
     </html>
   );
